67806ac3c46393988321bae67721ea2dbf55999d
MarkNzioka <markkivuva7@gmail.com>
2023-08-20 03:34:53 -0700

    New header file

--- main.h
+++ main.h
@@ -3,6 +3,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdarg.h>
+
+void handle_decimal(va_list args, int *count);
+void handle_unknown(const char modifier, int *count);
+void handle_percent(int *count);
+void handle_string(va_list args, int *count);
+void handle_char(va_list args, int *count);
 int _printf(const char *format, ...);
 
 #endif
